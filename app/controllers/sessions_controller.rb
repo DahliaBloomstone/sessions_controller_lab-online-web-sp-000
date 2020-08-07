bd38e1879cfa5051197313ed85b93d00f6e17580
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-07 21:53:20 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,27 +1,22 @@
 class SessionsController < ApplicationController
-
-def new
+  def new
   end
 
-#log in flow:
-def create
-  if params [:name] == "" || params[:name] == nil
-redirect_to login_path
-else
-    session[:name] = params[:name]
-  redirect_to '/'
-end
-end
+  def create
+    if params[:name] == "" || params[:name] == nil
+      redirect_to login_path
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
 
-#log out flow:
-def destroy
-  if session[:name]
-  session.delete :name
+  def destroy
+    if session[:name]
+      session.delete :name
+    end
+    redirect_to '/'
+  end
 end
-redirect_to '/'
-end
-
-end
-
 
 # rails generate controller Sessions new --no-helper --no-assets --no-test-framework --skip-routes --skip
