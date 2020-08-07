0709c36843f44b80abf994168c0e9ec85c9d0e68
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-07 21:47:38 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,13 @@ class SessionsController < ApplicationController
 def new
   end
 
+#log in flow:
 def create
   session[:username] = params[:username]
   redirect_to '/'
 end
 
+#log out flow:
 def destroy
   session.delete :username
 end
